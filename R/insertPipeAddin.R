5af415f72c0b18bcd4fc1edd7a4f12ccb9e43ad6
RemyHrz <Remy.HARAZIN@student.umons.ac.be>
2022-02-11 09:31:26 +0100

    shortcut package

---
+++ R/insertPipeAddin.R
@@ -0,0 +1,17 @@
+#' Insert \%in\%.
+#'
+#' Call this function as an addin to insert \code{ \%in\% } at the cursor position.
+#'
+#' @export
+insertPipeAddin <- function() {
+  rstudioapi::insertText(" %>.% ")
+}
+
+#' Insert \->\.
+#'
+#' Call this function as an addin to insert \code{ \->\ } at the cursor position.
+#'
+#' @export
+insertRevopAddin <- function() {
+  rstudioapi::insertText(" -> ")
+}
